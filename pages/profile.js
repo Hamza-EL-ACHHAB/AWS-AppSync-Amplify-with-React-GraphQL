772576f13ec39f5d0808d919c9d633d4e10f9767
Hamza-EL-ACHHAB <hamza.elachhab@usmba.ac.ma>
2023-07-11 22:11:28 +0100

    updated profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,26 @@
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { withSSRContext, Auth } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 import { useState, useEffect } from 'react';
 
-function Profile({ user }) {
-  if (!user) return null;
+function Profile() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    checkUser();
+  }, []);
 
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    setUser(user);
+  }
+  if (!user) return null;
   return (
     <div>
-      <h1 className="text-3xl font-semibold tracking-wide mt-6">Profile</h1>
+      <h1
+        className="text-3xl font-semibold tracking-wide
+            mt-6"
+      >
+        Profile
+      </h1>
       <h1 className="font-medium text-gray-500 my-2">
         Username: {user.username}
       </h1>
@@ -18,15 +31,4 @@ function Profile({ user }) {
     </div>
   );
 }
-
-export async function getServerSideProps(context) {
-  const { Auth } = withSSRContext(context);
-  try {
-    const user = await Auth.currentAuthenticatedUser();
-    return { props: { user } };
-  } catch (error) {
-    return { props: {} };
-  }
-}
-
 export default withAuthenticator(Profile);
